d2ebe675f9de9b5cb4a8269c378885431d943661
KhushAhuja <97459041+KhushAhuja@users.noreply.github.com>
2023-05-26 00:39:11 +0530

    Add files via upload

--- routes/problemRoutes.js
+++ routes/problemRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { getAllProblems, createProblem, getProblemById } = require('../controllers/problemController');
+const { getAllProblems, createProblem, getProblemById ,increment_subm_problem} = require('../controllers/problemController');
 const router = express.Router();
 
 router.get('/getProblems',getAllProblems)
 router.get('/get-problem/:id',getProblemById)
+router.post('/submission',increment_subm_problem)
 router.post('/create-problem',createProblem)
 
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { authUser, registerUser } = require('../controllers/userController');
+const { authUser, registerUser, increment_subm,updateprofilepic } = require('../controllers/userController');
 const { send_new_user } = require('../mailers/Signup');
 const router = express.Router();
 
  
 router.post('/login',authUser)
+router.post('/submission',increment_subm)
+// router.post('/updateProfilePic',updateprofilepic)
 router.post('/signup',registerUser)
 
 
