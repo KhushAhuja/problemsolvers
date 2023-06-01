7a4b8f1f951413cc2f8049016328ea10c2f1be5a
KhushAhuja <97459041+KhushAhuja@users.noreply.github.com>
2023-06-01 13:56:48 +0530

    Update 1v1.js

--- routes/1v1.js
+++ routes/1v1.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Problem = require("../models/problemModel");
 const User = require("../models/userModel");
-const EloRating = require("elo-rating");
+
 
 let savedCodes = []; // Array to store the saved codes
 
